d1c02c8fa9108e6a699077c4525a0838a0cb6ae0
SirrbAllen <36188476+SirrbAllen@users.noreply.github.com>
2018-03-09 20:22:29 +0000

    login and tech database changes

--- RazorProject/Controllers/RazorController.cs
+++ RazorProject/Controllers/RazorController.cs
@@ -14,23 +14,23 @@ namespace RazorProject.Controllers
     {
         private RazorDB db = new RazorDB();
 
+
+        public ActionResult Index()
+        {
+            return View(db.Technologies.ToList());
+        }
+
         public ActionResult Register()
         {
             return View();
             
         }
-
         [HttpPost]
-        public ActionResult Add(string email, string password)
+        public ActionResult Create(string email, string password)
         {
             db.Accounts.Add(new Account { Email = email, Password = password });
             db.SaveChanges();
             return RedirectToAction("Index");
-        }
-        
-        public ActionResult Index()
-        {
-            return View(db.Technologies.ToList());
         }
 
         public ActionResult Create()
