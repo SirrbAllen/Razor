777e44e135eca41714e938b00bb02a1a694e3788
Benjamin Allen <12allenb@utcsheffield.org.uk>
2018-03-14 15:33:07 +0000

    Register validation COMPLETE

--- RazorProject/Controllers/RazorController.cs
+++ RazorProject/Controllers/RazorController.cs
@@ -7,7 +7,9 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using RazorProject.Models;
+using System.Data.Entity.Infrastructure;
 using DevOne.Security.Cryptography.BCrypt;
+using System.Text.RegularExpressions;
 
 namespace RazorProject.Controllers
 {
@@ -28,23 +30,49 @@ namespace RazorProject.Controllers
         [HttpPost]
         public ActionResult Register(string email, string password)
         {
-            if (password.Length < 8)
-            {
-                ViewBag.ErrorMessage = "Password must be 8 or more characters";
-                return View();
-            }
-            if (password.Any(char.IsDigit))
-            {
-                ViewBag.ErrorMessage = "Password must contain a numerical character";
-                return View();
-            }
-            else
-            {
                 ViewBag.ErrorMessage = string.Empty;
-                db.Accounts.Add(new Account { Email = email, Password = password });
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
+                var hasNumber = new Regex(@"[0-9]+");
+                var hasUpperChar = new Regex(@"[A-Z]+");
+                var hasMinChars = new Regex(@".{8}");
+                var hasLowerChar = new Regex(@"[a-z]+");
+                var hasSymbols = new Regex(@"[!@#$%^&*()_+=\[{\]};:<>|./?,-]");
+
+                if (!hasLowerChar.IsMatch(password))
+                {
+                    ViewBag.ErrorMessage = ViewBag.ErrorMessage + " Password should contain At least one lower case letter.";
+                    return View();
+                }
+                if (!hasUpperChar.IsMatch(password))
+                {
+                    ViewBag.ErrorMessage = ViewBag.ErrorMessage + " Password should contain At least one upper case letter.";
+                    return View();
+                }
+                if (!hasMinChars.IsMatch(password))
+                {
+                    ViewBag.ErrorMessage = ViewBag.ErrorMessage + " Password should contain at least eight characters.";
+                    return View();
+                }
+                if (!hasNumber.IsMatch(password))
+                {
+                    ViewBag.ErrorMessage = ViewBag.ErrorMessage + " Password should contain At least one numeric value.";
+                    return View();
+                }
+                if (!hasSymbols.IsMatch(password))
+                {
+                    ViewBag.ErrorMessage = ViewBag.ErrorMessage + " Password should contain At least one special case characters.";
+                    return View();
+                }
+                if (ViewBag.ErrorMessage == string.Empty)
+                {
+                    ViewBag.ErrorMessage = string.Empty;
+                    db.Accounts.Add(new Account { Email = email, Password = password });
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return Content("Whoops!");
+                }
         }
 
         public ActionResult Create()
@@ -77,7 +105,7 @@ namespace RazorProject.Controllers
             }
             return View(technology);
         }
-        [HttpPost]
+        [HttpPost, ActionName("Edit")]
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "ID,Title,Description,Url,Priority,Category,Status")] Technology technology)
         {
